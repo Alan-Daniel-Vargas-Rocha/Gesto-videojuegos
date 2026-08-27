43dabf737ca4a1ea843404d408b47d3c1c4d9e99
Alantonic <alanvaro1117@gmail.com>
2026-08-27 15:41:53 -0700

    Vite corregido uso de dirección relativo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Gestor-videojuegos/', // ← DEBE COINCIDIR EXACTAMENTE CON EL NOMBRE DE TU REPO
+  base: './', // ← DEBE COINCIDIR EXACTAMENTE CON EL NOMBRE DE TU REPO
 })
