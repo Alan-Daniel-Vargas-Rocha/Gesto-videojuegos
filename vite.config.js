920fe173cc06ba82b01afe67971084dee545913f
Alantonic <alanvaro1117@gmail.com>
2026-08-27 15:35:37 -0700

    Vite configurado

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: '/Gestor-videojuegos/', // ← DEBE COINCIDIR EXACTAMENTE CON EL NOMBRE DE TU REPO
 })
