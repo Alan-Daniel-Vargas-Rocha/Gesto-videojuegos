7217eeb4853495be113cd98bd0886356d612c498
Alantonic <alanvaro1117@gmail.com>
2026-08-27 13:52:00 -0700

    Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Gestor-videojuegos/',
+  base: process.env.NODE_ENV === 'production' ? '/Gestor-videojuegos/' : '/',
   plugins: [react()],
 })
